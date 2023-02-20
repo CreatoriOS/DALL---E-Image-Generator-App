9786c3da98170560f460db3e1c2e6567bfae9221
ZHANSAYA BORTANOVA <123213674+CreatoriOS@users.noreply.github.com>
2023-02-20 13:58:51 +0400

    Add files via upload

---
+++ ImageGeneratorApp/ImageGeneratorApp/ImageGeneratorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGeneratorAppApp.swift
+//  ImageGeneratorApp
+//
+//  Created by Zhansaya Bortanova on 20/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ImageGeneratorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
